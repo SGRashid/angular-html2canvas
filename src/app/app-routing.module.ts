e1b88e6b4c499a8dd1581c3f76231ab839c87bab
Sergei Gabdullin <s.gabdullin@gmail.com>
2022-04-05 21:25:47 +0500

    Iframe component works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CssComponent } from './css/css.component';
+import { IframeComponent } from './iframe/iframe.component';
 import { NewComponentComponent } from './new-component/new-component.component';
 import { PromiseComponent } from './promise/promise.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', component: NewComponentComponent },
   { path: 'promise', component: PromiseComponent },
   { path: 'css', component: CssComponent },
+  { path: 'iframe', component: IframeComponent },
 ];
 
 @NgModule({
