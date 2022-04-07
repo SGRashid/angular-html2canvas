b9f03ead571b01806bdba3d5429f59ff63719458
Sergei Gabdullin <s.gabdullin@gmail.com>
2022-04-07 17:17:42 +0500

    shadow dom works

--- src/app/shadow-dom/shadow-dom.component.ts
+++ src/app/shadow-dom/shadow-dom.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { ScreenshotService } from '../screenshot.service';
 
 @Component({
   selector: 'app-shadow-dom',
   templateUrl: './shadow-dom.component.html',
   styleUrls: ['./shadow-dom.component.css']
 })
-export class ShadowDomComponent implements OnInit {
+export class ShadowDomComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor(public screenshotService: ScreenshotService) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+
+    class ShadowDOMElement extends HTMLElement { // (1)
+
+      connectedCallback() {
+        const shadow = this.attachShadow({mode: 'open'});
+        shadow.innerHTML = '<p>Shadow DOM</p>';
+      }
+      
+    }
+      
+      customElements.define("shadow-dom", ShadowDOMElement); // (2)
+      const newElement = document.createElement('shadow-dom');
+      document.body.appendChild(newElement);
+  }
+
+  makeScreenshot(): void {
+    this.screenshotService.makeScreenshot();
   }
 
 }
